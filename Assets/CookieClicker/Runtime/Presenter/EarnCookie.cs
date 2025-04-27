4f6aa11cccf18f62db15b5758661cdb41c644e1c
kahsez <kahsez@users.noreply.github.com>
2025-04-27 11:32:08 +0200

    test: earn cookie adds one cookie to jar

---
+++ Assets/CookieClicker/Runtime/Presenter/EarnCookie.cs
@@ -0,0 +1,17 @@
+﻿using CookieClicker.Runtime.Model;
+
+namespace CookieClicker.Runtime.Presenter
+{
+	public class EarnCookie
+	{
+		public EarnCookie(Jar jar)
+		{
+
+		}
+
+		public void Execute()
+		{
+
+		}
+	}
+}
---
+++ Assets/CookieClicker/Tests/EarnCookieTests.cs
@@ -0,0 +1,21 @@
+﻿using CookieClicker.Runtime.Model;
+using CookieClicker.Runtime.Presenter;
+using NUnit.Framework;
+
+namespace CookieClicker.Tests
+{
+	[TestFixture]
+	public class EarnCookieTests
+	{
+		[Test]
+		public void EarnCookieAddsOneCookieToJar()
+		{
+			var doc = new Jar();
+			var sut = new EarnCookie(doc);
+
+			sut.Execute();
+
+			Assert.That(doc.Amount(), Is.EqualTo(1));
+		}
+	}
+}
