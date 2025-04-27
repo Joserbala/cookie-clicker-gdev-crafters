2b259da40fb902ea374e24749731f2ecbd3949db
kahsez <kahsez@users.noreply.github.com>
2025-04-27 12:31:24 +0200

    test: Earn Cookie refreshes the view

--- Assets/CookieClicker/Tests/EarnCookieTests.cs
+++ Assets/CookieClicker/Tests/EarnCookieTests.cs
@@ -17,5 +17,33 @@ namespace CookieClicker.Tests
 
 			Assert.That(doc.Amount(), Is.EqualTo(1));
 		}
+
+		[Test]
+		public void EarnCookieUpdatesCookieCounter()
+		{
+			var doc = new Jar();
+			var doc2 = new MockView();
+			var sut = new EarnCookie(doc);
+
+			sut.Execute();
+
+			Assert.That(doc2.hasBeenRefreshed, Is.EqualTo(true));
+		}
+
+		public interface ICookieCounterView
+		{
+			void Refresh();
+		}
+
+		public class MockView : ICookieCounterView
+		{
+			public bool hasBeenRefreshed = false;
+			public void Refresh()
+			{
+				hasBeenRefreshed = true;
+			}
+		}
 	}
+
+
 }
