3ceb0ffd373a91cdce7698d152558eb31f9658b6
kahsez <kahsez@users.noreply.github.com>
2025-04-27 11:34:19 +0200

    feat: earn cookie adds one cookie to jar

--- Assets/CookieClicker/Runtime/Presenter/EarnCookie.cs
+++ Assets/CookieClicker/Runtime/Presenter/EarnCookie.cs
@@ -4,14 +4,16 @@ namespace CookieClicker.Runtime.Presenter
 {
 	public class EarnCookie
 	{
+		readonly Jar jar;
+
 		public EarnCookie(Jar jar)
 		{
-
+			this.jar = jar;
 		}
 
 		public void Execute()
 		{
-
+			jar.Add();
 		}
 	}
 }
