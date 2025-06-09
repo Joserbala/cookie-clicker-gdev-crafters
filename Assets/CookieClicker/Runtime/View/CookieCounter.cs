9127d81f4761b0a016d93e7311c3cadf3b85fc45
kahsez <kahsez@users.noreply.github.com>
2025-06-09 11:23:17 +0200

    feat: create CookieCounter view

---
+++ Assets/CookieClicker/Runtime/View/CookieCounter.cs
@@ -0,0 +1,16 @@
+﻿using CookieClicker.Runtime.Presenter;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace CookieClicker.Runtime.View
+{
+	public class CookieCounter : MonoBehaviour, ICookieCounterView
+	{
+		[SerializeField] Text text;
+
+		public void Refresh()
+		{
+
+		}
+	}
+}
