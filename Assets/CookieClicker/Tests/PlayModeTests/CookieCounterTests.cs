aa7cb4640de31d7ddbc478fa8121db01c4a7ed32
Joser Gala Naranjo <jgalanar@gmail.com>
2025-06-09 12:36:37 +0200

    test(view): clicking once earns one cookie

---
+++ Assets/CookieClicker/Tests/PlayModeTests/CookieCounterTests.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using CookieClicker.Runtime.View;
+using NUnit.Framework;
+using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.TestTools;
+using UnityEngine.UI;
+
+namespace CookieClicker.Tests.PlayModeTests
+{
+	public class CookieCounterTests
+	{
+		[UnityTest]
+		public IEnumerator ClickingOnceEarnsOneCookie()
+		{
+			yield return SceneManager.LoadSceneAsync(0);
+			yield return null;
+
+			UnityEngine.Object.FindAnyObjectByType<Button>().onClick.Invoke();
+
+			Assert.That(UnityEngine.Object.FindAnyObjectByType<CookieCounter>().GetComponent<TMP_Text>().text,
+				Is.EqualTo("1"));
+		}
+	}
+}
