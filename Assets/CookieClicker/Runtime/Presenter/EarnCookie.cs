00545892b0066772d793125d7bebff18062e8cf1
kahsez <kahsez@users.noreply.github.com>
2025-06-09 11:16:16 +0200

    feat: Earn Cookie refreshes the view

--- Assets/CookieClicker/Runtime/Presenter/EarnCookie.cs
+++ Assets/CookieClicker/Runtime/Presenter/EarnCookie.cs
@@ -5,15 +5,18 @@ namespace CookieClicker.Runtime.Presenter
 	public class EarnCookie
 	{
 		readonly Jar jar;
+		readonly ICookieCounterView view;
 
 		public EarnCookie(Jar jar, ICookieCounterView view)
 		{
 			this.jar = jar;
+			this.view = view;
 		}
 
 		public void Execute()
 		{
 			jar.Add();
+			view.Refresh();
 		}
 	}
 }
