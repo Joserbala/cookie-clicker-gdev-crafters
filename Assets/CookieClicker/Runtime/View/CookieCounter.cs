144a038b11268030a9498ba6a3b9fb70f635d4bc
kahsez <kahsez@users.noreply.github.com>
2025-06-09 12:06:44 +0200

    feat: Earn cookie with button

--- Assets/CookieClicker/Runtime/View/CookieCounter.cs
+++ Assets/CookieClicker/Runtime/View/CookieCounter.cs
@@ -1,12 +1,23 @@
-﻿using CookieClicker.Runtime.Presenter;
+﻿using System;
+using CookieClicker.Runtime.Model;
+using CookieClicker.Runtime.Presenter;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace CookieClicker.Runtime.View
 {
 	public class CookieCounter : MonoBehaviour, ICookieCounterView
 	{
 		[SerializeField] TMP_Text text;
+		[SerializeField] Button cookieButton;
+
+		void Awake()
+		{
+			var jar = new Jar();
+			var earnCookie = new EarnCookie(jar, this);
+			cookieButton.onClick.AddListener(earnCookie.Execute);
+		}
 
 		public void Refresh(int amount)
 		{
