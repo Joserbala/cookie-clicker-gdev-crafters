752d6fbc52ccf31650a522ec7b06727623ac53f9
Joser Gala Naranjo <jgalanar@gmail.com>
2025-06-15 13:36:47 +0200

    test: after passing time view displays 1 cookie

--- Assets/CookieClicker/Tests/EditModeTests/EarnCookieTests.cs
+++ Assets/CookieClicker/Tests/EditModeTests/EarnCookieTests.cs
@@ -48,11 +48,24 @@ namespace CookieClicker.Tests.EditModeTests
 		public void PassTime()
 		{
 			var doc = new Jar();
-			var sut = new PassTime(doc);
+			var doc2 = new MockCookieCounterView();
+			var sut = new PassTime(doc, doc2);
 
 			sut.Execute(1.2f);
 
 			Assert.That(doc.Amount, Is.EqualTo(1));
+		}
+
+		[Test]
+		public void AfterPassingTimeViewDisplays1Cookie()
+		{
+			var doc = new Jar();
+			var doc2 = new MockCookieCounterView();
+			var sut = new PassTime(doc, doc2);
+
+			sut.Execute(1.2f);
+
+			Assert.That(doc2.Counter, Is.EqualTo(1));
 		}
 	}
 
@@ -60,7 +73,7 @@ namespace CookieClicker.Tests.EditModeTests
 	{
 		readonly Jar jar;
 
-		public PassTime(Jar jar)
+		public PassTime(Jar jar, ICookieCounterView doc2)
 		{
 			this.jar = jar;
 		}
