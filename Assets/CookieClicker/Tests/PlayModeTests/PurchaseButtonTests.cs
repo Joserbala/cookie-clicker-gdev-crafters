baa4f179435e17ef82724594a511becd2bd8bc98
Joser Gala Naranjo <jgalanar@gmail.com>
2025-08-04 18:55:05 +0200

    test: autoclicker button is disabled by default

---
+++ Assets/CookieClicker/Tests/PlayModeTests/PurchaseButtonTests.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using CookieClicker.Runtime.View;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.TestTools;
+using UnityEngine.UI;
+
+namespace CookieClicker.Tests.PlayModeTests
+{
+	public class PurchaseButtonTests
+	{
+		[UnityTest]
+		public IEnumerator Sasdgafsdg()
+		{
+
+			yield return SceneManager.LoadSceneAsync(0);
+			yield return null;
+
+			var autoClickerButton = Object.FindAnyObjectByType<PurchaseAutoclickerButton>().GetComponent<Button>();
+
+			Assert.That(autoClickerButton.enabled, Is.False);
+		}
+	}
+}
