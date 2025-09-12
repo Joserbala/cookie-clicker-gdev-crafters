c4e03c4fa7df7b6f52874b4ea873edfece578547
kahsez <kahsez@users.noreply.github.com>
2025-09-12 18:14:41 +0200

    test: purchase autoclicker button is not interactable by default

--- Assets/CookieClicker/Tests/PlayModeTests/PurchaseButtonTests.cs
+++ Assets/CookieClicker/Tests/PlayModeTests/PurchaseButtonTests.cs
@@ -1,6 +1,24 @@
-﻿namespace CookieClicker.Tests.PlayModeTests
+﻿using System.Collections;
+using CookieClicker.Runtime.View;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.TestTools;
+using UnityEngine.UI;
+
+namespace CookieClicker.Tests.PlayModeTests
 {
 	public class PurchaseButtonTests
 	{
+		[UnityTest]
+		public IEnumerator PurchaseAutoclickerButtonIsNotInteractableByDefault()
+		{
+			yield return SceneManager.LoadSceneAsync(0);
+			yield return null;
+
+			var button = Object.FindAnyObjectByType<PurchaseAutoclickerButton>().GetComponent<Button>();
+
+			Assert.That(button.interactable, Is.False);
+		}
 	}
 }
